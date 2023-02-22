84e3d9c29e6a4d22995f0c5da024df76e03551bc
militisofverum <militisofverum@gmail.com>
2023-02-22 14:54:56 +0200

    add balance checking

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def webhook():
             buy_receive(coin, stableCoin, pair)
         if message == 'SELL':
             sell_receive(coin, stableCoin, pair)
+        if message == 'balance':
+            balance = get_balance(coin)
+            stableCoinBalance = get_balance(stableCoin)
+            send_telegram_message("balance Coin: " + str(balance) + "balance stable Coin: " + str(stableCoinBalance))
     except Exception as e: 
         send_telegram_message("Error:" + str(e))
     return 'hello to me'
